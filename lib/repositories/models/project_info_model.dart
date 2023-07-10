22c9fda621b588b0ea13009fccd2a6b3b98315f9
limnull <m.3098.off@gmail.com>
2023-07-10 22:26:49 +0300

    создание модели project_info

--- lib/repositories/models/project_info_model.dart
+++ lib/repositories/models/project_info_model.dart
@@ -1,1 +1,13 @@
-class ProjectInfoModel {}
+import 'package:equatable/equatable.dart';
+
+class ProjectInfoModel extends Equatable {
+  final int? projectNumber;
+  final String? projectId;
+  final String? storageBucket;
+
+  const ProjectInfoModel(
+      {this.projectNumber, this.projectId, this.storageBucket});
+
+  @override
+  List<Object?> get props => [projectNumber, projectId, storageBucket];
+}
