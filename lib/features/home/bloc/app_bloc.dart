fb2bba9f53607f417d4fa1afc2694225b6cfd905
limnull <m.3098.off@gmail.com>
2023-07-11 06:49:11 +0300

    Создание тествого задания оконченно

--- lib/features/home/bloc/app_bloc.dart
+++ lib/features/home/bloc/app_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+
 import 'package:get_it/get_it.dart';
 import 'package:test_app/repositories/app_repository.dart';
 import 'package:webview_flutter/webview_flutter.dart';
@@ -11,14 +12,15 @@ class AppBloc extends Bloc<AppEvent, AppState> {
   AppBloc() : super(AppInitial()) {
     on<OpenWebView>((event, emit) async {
       emit(AppLoading());
-
       try {
         final Uri uri =
             await GetIt.I<AppRepositroy>().getUri("Dmitry", "Smirnov");
         final WebViewController webViewController = WebViewController()
           ..setJavaScriptMode(JavaScriptMode.unrestricted)
           ..loadRequest(uri);
-        await Future.delayed(const Duration(seconds: 1));
+
+        await Future.delayed(
+            const Duration(seconds: 1)); //имитация подгрузки api
 
         emit(AppLoaded(webViewController));
       } catch (e) {
--- lib/features/home/view/home_screen.dart
+++ lib/features/home/view/home_screen.dart
@@ -1,10 +1,43 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:webview_flutter_android/webview_flutter_android.dart';
 import '../bloc/app_bloc.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late WebViewController controller;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void addFileSelectionListener() async {
+    if (Platform.isAndroid) {
+      final androidController = controller.platform as AndroidWebViewController;
+      await androidController.setOnShowFileSelector(_androidFilePicker);
+    }
+  }
+
+  Future<List<String>> _androidFilePicker(
+      final FileSelectorParams params) async {
+    final result = await FilePicker.platform.pickFiles();
+
+    if (result != null && result.files.single.path != null) {
+      final file = File(result.files.single.path!);
+      return [file.uri.toString()];
+    }
+    return [];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,8 +57,10 @@ class HomeScreen extends StatelessWidget {
             body: Center(child: CircularProgressIndicator()),
           );
         } else if (state is AppLoaded) {
+          controller = state.webViewController;
+          addFileSelectionListener();
           return Scaffold(
-            body: WebViewWidget(controller: state.webViewController),
+            body: WebViewWidget(controller: controller),
           );
         } else if (state is AppLoadingFailed) {
           return Scaffold(
